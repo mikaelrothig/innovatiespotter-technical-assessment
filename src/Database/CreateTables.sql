6fbe5d1a98c5f2ded7c821ad31d7487e709e763b
MikaelRothig <mrrothig@gmail.com>
2025-03-26 17:14:05 +0200

    Updated SQL queries

--- src/Database/CreateTables.sql
+++ src/Database/CreateTables.sql
@@ -1,14 +1,16 @@
-CREATE TABLE companies (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    website VARCHAR(255),
-    address TEXT,
-    source VARCHAR(50),
+CREATE TABLE companies
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255),
+    website     VARCHAR(255),
+    address     TEXT,
+    source      VARCHAR(50),
     inserted_at TIMESTAMP DEFAULT NOW()
 );
-CREATE TABLE normalized_companies (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE,
+CREATE TABLE normalized_companies
+(
+    id                SERIAL PRIMARY KEY,
+    name              VARCHAR(255) UNIQUE,
     canonical_website VARCHAR(255),
-    address TEXT
+    address           TEXT
 );
